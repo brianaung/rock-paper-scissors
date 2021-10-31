76bd8723c09b8a71a66568d30b3dad01a7703bc8
Brian Aung <soekhantaung16@gmail.com>
2021-11-01 01:30:20 +1100

    implement game logic and play the game

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ function comPlay() {
   return shapeArr[randomNum];
 }
 
-console.log(comPlay());
+/* play a game of rock, paper, scissors */
+function playRound(pSelect, cSelect) {
+  let res = '';
+  pSelect = pSelect.toLowerCase();
+  cSelect = cSelect.toLowerCase();
+
+  /* compare the shapes played by player and computer and 
+   * decide the winner */
+  if (pSelect === cSelect) {
+    res = 'Draw!';
+  } else if (pSelect === 'rock' && cSelect === 'paper' ||
+            pSelect === 'paper' && cSelect === 'scissors' ||
+            pSelect === 'scissors' && cSelect === 'rock') {
+    res = `You lost! You played ${pSelect} and the computer played ${cSelect}.`;
+  } else {
+    res = `You won! You played ${pSelect} and the computer played ${cSelect}.`;
+  }
+  return res;
+}
+
+/* get player input, randomise comp input, and call play function */
+const pSelect = prompt('Choose what you want to play (rock, paper, scissors): ');
+const cSelect = comPlay();
+console.log(playRound(pSelect, cSelect));
